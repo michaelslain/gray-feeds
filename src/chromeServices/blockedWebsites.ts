aafdaca2d5c1464f3e02d05c2acb01f6070cb1f4
michaelslain <misha.slain@icloud.com>
2023-06-05 09:47:12 -0700

    Updated blocked websites

--- src/chromeServices/blockedWebsites.ts
+++ src/chromeServices/blockedWebsites.ts
@@ -4,9 +4,6 @@ const BLOCKED_WEBSITES: string[] = [
     'https://www.instagram.com',
     'https://www.reddit.com',
     'https://www.twitter.com',
-    'https://www.tumblr.com',
-    'https://www.pinterest.com',
-    'https://www.linkedin.com',
     'https://www.netflix.com',
     'https://www.twitch.tv',
 ]
